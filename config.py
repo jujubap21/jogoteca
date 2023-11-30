c09d0a565136b6d7a20496fcb9360452814ddaed
Júlia Pfister <148919434+jujubap21@users.noreply.github.com>
2023-11-30 16:58:44 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+SECRET_KEY = "alura"
+
+SQLALCHEMY_DATABASE_URI = "{SGBD}://{usuario}:{senha}@{servidor}/{database}".format(
+    SGBD="mysql+mysqlconnector",
+    usuario="root",
+    senha="admin",
+    servidor="localhost",
+    database="jogoteca",
+)
+
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + "/uploads"
---
+++ prepara_banco.py
@@ -0,0 +1,88 @@
+import mysql.connector
+from mysql.connector import errorcode
+from flask_bcrypt import generate_password_hash
+
+print("Conectando...")
+try:
+    conn = mysql.connector.connect(host="127.0.0.1", user="root", password="admin")
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Existe algo errado no nome de usuário ou senha")
+    else:
+        print(err)
+
+cursor = conn.cursor()
+
+cursor.execute("DROP DATABASE IF EXISTS `jogoteca`;")
+
+cursor.execute("CREATE DATABASE `jogoteca`;")
+
+cursor.execute("USE `jogoteca`;")
+
+TABLES = {}
+TABLES[
+    "Jogos"
+] = """
+      CREATE TABLE `jogos` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `nome` varchar(50) NOT NULL,
+      `categoria` varchar(40) NOT NULL,
+      `console` varchar(20) NOT NULL,
+      PRIMARY KEY (`id`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;"""
+
+TABLES[
+    "Usuarios"
+] = """
+      CREATE TABLE `usuarios` (
+      `nome` varchar(20) NOT NULL,
+      `nickname` varchar(8) NOT NULL,
+      `senha` varchar(100) NOT NULL,
+      PRIMARY KEY (`nickname`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;"""
+
+for tabela_nome in TABLES:
+    tabela_sql = TABLES[tabela_nome]
+    try:
+        print("Criando tabela {}:".format(tabela_nome), end=" ")
+        cursor.execute(tabela_sql)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("Já existe")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
+
+
+usuario_sql = "INSERT INTO usuarios (nome, nickname, senha) VALUES (%s, %s, %s)"
+usuarios = [
+    ("Júlia Pfister", "Jules", generate_password_hash("alohomora").decode("utf-8")),
+]
+cursor.executemany(usuario_sql, usuarios)
+
+cursor.execute("select * from jogoteca.usuarios")
+print(" -------------  Usuários:  -------------")
+for user in cursor.fetchall():
+    print(user[1])
+
+jogos_sql = "INSERT INTO jogos (nome, categoria, console) VALUES (%s, %s, %s)"
+jogos = [
+    ("Persona 3 Portable", "JRPG", "PSP"),
+    ("Life is Strange", "Aventura", "PS4"),
+    ("The Witcher 3", "RPG", "PC"),
+    ("Baldur's Gate 3", "RPG", "PC"),
+    ("Vampire The Maquerade - Bloodlines", "RPG", "PC"),
+    ("God of War 3", "Hack n Slash", "PS3"),
+]
+cursor.executemany(jogos_sql, jogos)
+
+cursor.execute("select * from jogoteca.jogos")
+print(" -------------  Jogos:  -------------")
+for jogo in cursor.fetchall():
+    print(jogo[1])
+
+conn.commit()
+
+cursor.close()
+conn.close()
